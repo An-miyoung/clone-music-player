04e2c3e8fb47acad4c9ecf934bcdf102e165f46d
miyoung <noonchicat@gmail.com>
2022-09-16 21:38:16 +0900

    feat: ref를 forward  해서 progressBar 구현"

--- src/components/PrograssArea/ProgressArea.jsx
+++ src/components/PrograssArea/ProgressArea.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useImperativeHandle, useRef, forwardRef } from "react";
 import "./ProgressArea.scss";
+
 function ProgressArea(props, ref) {
+  // 실제 핸들링되는 ref: 새로운 ref 선언해서 js 가 제공하는 audio 에 붙인다.
+  const audioRef = useRef();
+
+  // 엡에서 내려준 ref를 받는다.
+  useImperativeHandle(ref, () => ({
+    play: () => {
+      audioRef.current.play();
+    },
+    pause: () => {
+      audioRef.current.pause();
+    },
+  }));
   return (
     <div className="progress-area">
       <div className="progress-bar">
-        <audio autoPlay></audio>
+        <audio autoPlay ref={audioRef}></audio>
       </div>
       <div className="music-timer">
         <span>00:00</span>
@@ -14,4 +27,4 @@ function ProgressArea(props, ref) {
   );
 }
 
-export default ProgressArea;
+export default forwardRef(ProgressArea);
